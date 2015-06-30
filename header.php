f71cdf42577139a21c6f17a7f475d1c876fef595
PMyers1732 <go_military_usa.pm111@yahoo.com>
2015-06-30 15:37:24 -0500

    changed files to pdfs. added a header file and footer file. home header is home folder.

---
+++ header.php
@@ -0,0 +1,35 @@
+<?php
+
+?>
+<!-- Header -->
+				<div id="header-wrapper">
+					<div class="container">
+
+						<!-- Header -->
+							<header id="header">
+								<div class="inner">
+
+									<!-- Logo -->
+										<h1><a href="index.php" id="logo">F-2 Foxes</a></h1>
+
+									<!-- Nav -->
+										<nav id="nav">
+											<ul>
+												<li class="current_page_item"><a href="index.php">Home</a></li>
+												
+												
+                                                <li><a href="about.php">About Us</a></li>
+                                                <li><a href="events.php">Events</a></li>
+                                                <li><a href="recruiting.php">Join F-2</a></li>
+                                                <li><a href="history.php">Contact Us</a></li>
+                                                <li><a href="login.php">Log In</a></li>
+                                                <li><a href="signup.php">Sign Up</a></li>
+												
+                                                
+											</ul>
+										</nav>
+
+								</div>
+							</header>
+					</div>
+				</div>
---
+++ history.php
@@ -0,0 +1,77 @@
+<?php
+
+?>
+
+<!DOCTYPE HTML>
+<!--
+	ZeroFour by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>events</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body class="no-sidebar">
+		<div id="page-wrapper">
+
+            <!--header-->
+            <?php include 'header.php'; ?>
+            <!--end of header-->
+
+			<!-- Main Wrapper -->
+				<div id="main-wrapper">
+					<div class="wrapper style2">
+						<div class="inner">
+							<div class="container">
+								<div id="content">
+
+
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="wrapper style3">
+						<div class="inner">
+							<div class="container">
+								<div class="row">
+									<div class="8u 12u(mobile)">
+									</div>
+                                    <h1> Description of the past of fox company</h1>
+                                    <p> Include some nice pictures of the crest and some current pictures</p>
+									<div class="4u 12u(mobile)">
+
+
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			
+			<!-- Footer  -->
+            <?php include 'footer.php'; ?>
+            <!--end of Footer-->
+				
+
+		</div>
+
+		<!-- Scripts -->
+
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/skel-viewport.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
