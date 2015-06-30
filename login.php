7a0c9d041cde14344af17b540b159d3e05b26527
dtmcderm <dtmcderm@tamu.edu>
2015-06-30 13:19:16 -0500

    fixed mobile title bar from covering up content for login.php

--- login.php
+++ login.php
@@ -58,6 +58,9 @@
 						<div class="inner">
 							<div class="container">
 								<div id="content">
+                                     <!--Prevents Mobile Nav Bar From covering up content-->
+                                    <br></br>
+                                    <h1>Please Log In</h1>
                                     <form action= "login_submit.php" method="post">
                                     <p>
                                     <label for="user">Username</label>
